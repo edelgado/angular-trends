969e4041c8a20609700b9d70349aa2306c42945e
Enrique Delgado <delgado.enrique@gmail.com>
2014-03-08 17:46:16 -0600

    Start using both Angular and the Node.js server.

--- app/trends.js
+++ app/trends.js
@@ -3,18 +3,14 @@ angular.module('trends', [])
 .factory('dataFactory', function($http) {
   var factory = {};
   factory.getTrendData = function() {
-    var dataFunc = $http.get('http://www.google.com/trends/fetchComponent?q=angular,backbone,sproutcore&cid=TIMESERIES_GRAPH_0&export=3');
-    dataFunc();
-    var something = {
-      date: '2010-03',
-      value: 100
-    };
-    return something;
+    // var dataFunc = $http.get('http://www.google.com/trends/fetchComponent?q=angular,backbone,sproutcore&cid=TIMESERIES_GRAPH_0&export=3');
+    // The data comes from Google's http://www.google.com/trends/fetchComponent URI, via a small node.js service.
+    return $http.get('http://0.0.0.0:3000/data.json');
   }
   return factory;
 })
 
-.controller('tendsController', function($scope, dataFactory) {
+.controller('trendsController', function($scope, dataFactory) {
   $scope.trendData = dataFactory.getTrendData();
     console.log('hello?');
     return data;
